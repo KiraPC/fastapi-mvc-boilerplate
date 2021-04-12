fa364045552f2c3379d0a61f3f577cf9a0323a33
KiraPC <pasqualecarmine.carbone@gmail.com>
2021-04-12 12:52:20 +0200

    use orjson

--- server/schema/errors.py
+++ server/schema/errors.py
@@ -1,15 +1,17 @@
 from fastapi import status
+from dataclasses import dataclass
 from pydantic import BaseModel, Field
-from fastapi.responses import JSONResponse
+from fastapi.responses import ORJSONResponse
 
 class ErrorModel(BaseModel):
     error: str = Field(..., description='The error message description')
 
-class ErrorResponse(dict):
+@dataclass
+class ErrorResponse():
     def __init__(self, message):
-        dict.__init__(self, error=message)
+        self.error = message
 
 class Errors():
-    HTTP_500_INTERNAL_SERVER_ERROR = JSONResponse(ErrorResponse('Internal Server Error'), status.HTTP_500_INTERNAL_SERVER_ERROR)
-    HTTP_404_NOT_FOUND = JSONResponse(ErrorResponse('Content not found on DB'), status.HTTP_404_NOT_FOUND)
-    HTTP_400_BAD_REQUEST = lambda message: JSONResponse(ErrorResponse(message), status.HTTP_400_BAD_REQUEST)
+    HTTP_500_INTERNAL_SERVER_ERROR = ORJSONResponse(ErrorResponse('Internal Server Error'), status.HTTP_500_INTERNAL_SERVER_ERROR)
+    HTTP_404_NOT_FOUND = ORJSONResponse(ErrorResponse('Content not found on DB'), status.HTTP_404_NOT_FOUND)
+    HTTP_400_BAD_REQUEST = lambda message: ORJSONResponse(ErrorResponse(message), status.HTTP_400_BAD_REQUEST)
